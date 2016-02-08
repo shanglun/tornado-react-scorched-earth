1b6874bb5357589372db030aecb572915c6178df
shanglun <shanglunwang@gmail.com>
2016-02-07 21:35:20 -0800

    run game while browser not in focus

--- client/src/tank/phaser/main.js
+++ client/src/tank/phaser/main.js
@@ -2,13 +2,14 @@ export default {
   preload: function() {
 
 		this.stage.backgroundColor = '#04D90E'//'#B4D9E7';
+    this.stage.disableVisibilityChange = true; //run while not in focus
 
   },
   create: function() {
 
   },
   update: function() {
-  
+
   },
   mousemove: function(pointer, x, y) {
 
